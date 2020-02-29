02415e3dc27e79fb1346da22eb37c5748404aa73
chappidinavitha <59575194+chappidinavitha@users.noreply.github.com>
2020-02-29 04:05:00 -0800

    GIFT CARD
    worked on navbar,sidebar and model

---
+++ model/gift.model.ts
@@ -0,0 +1,15 @@
+export class Order{
+    constructor(
+    public firstname: string,
+    public lastname: string,
+    public  inr: number,
+    public paise:number,
+    public address1: string,
+    public address2:string,
+    public bFname : string,
+    public bLname : string,
+    public email: string,
+    public phone: number
+
+    ){}
+}
